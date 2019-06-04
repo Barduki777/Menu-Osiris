a447bdf04235a01f41b384dcbace5281d8a930a2
barduki <44278741+Barduki777@users.noreply.github.com>
2019-06-04 13:08:38 -0300

    Update GUI.h

--- Osiris/GUI.h
+++ Osiris/GUI.h
@@ -11,29 +11,11 @@ private:
     static void hotkey(int&) noexcept;
     void renderMenuBar() noexcept;
 	void MainMenu()  noexcept;
-    void renderAimbotWindow() noexcept;
-    void renderTriggerbotWindow() noexcept;
-    void renderBacktrackWindow() noexcept;
-    void renderGlowWindow() noexcept;
-    void renderChamsWindow() noexcept;
-    void renderEspWindow() noexcept;
-    void renderVisualsWindow() noexcept;
-    void renderSkinChangerWindow() noexcept;
-    void renderMiscWindow() noexcept;
-    void renderConfigWindow() noexcept;
+	
 
     struct {
 		bool MainMenu{ false };
-        bool aimbot{ false };
-        bool triggerbot{ false };
-        bool backtrack{ false };
-        bool glow{ false };
-        bool chams{ false };
-        bool esp{ false };
-        bool visuals{ false };
-        bool skinChanger{ false };
-        bool misc{ false };
-        bool config{ false };
+
     } window;
 };
 
